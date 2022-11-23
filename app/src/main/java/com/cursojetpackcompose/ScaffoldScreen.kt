018ca8e18506bf140dde9905281577ea26951215
Julian Cuestas Jimenez <julian.cuestas@sii-group.co>
2022-11-22 21:45:42 -0500

    Se agrega ejercicio practico de uso de Snackbar en los Scaffold

--- app/src/main/java/com/cursojetpackcompose/ScaffoldScreen.kt
+++ app/src/main/java/com/cursojetpackcompose/ScaffoldScreen.kt
@@ -6,36 +6,47 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
 fun MyScaffold() {
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
     Scaffold(
-        topBar = { MyTopAppBar() }
+        topBar = { MyTopAppBar {
+                coroutineScope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar("Has pulsado $it")
+                }
+            }
+        },
+        scaffoldState = scaffoldState
     ) {
 
     }
 }
 
 @Composable
-fun MyTopAppBar() {
+fun MyTopAppBar(onClickIcon: (String) -> Unit) {
     TopAppBar(
         title = { Text(text = "Mi primera topbar") },
         backgroundColor = Color.Blue,
         contentColor = Color.White,
         elevation = 4.dp,
         navigationIcon = {
-            IconButton(onClick = {  }) {
+            IconButton(onClick = { onClickIcon("Atrás") }) {
                 Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back")
             }
         },
         actions = {
-            IconButton(onClick = {  }) {
+            IconButton(onClick = { onClickIcon("Buscar") }) {
                 Icon(imageVector = Icons.Filled.Search, contentDescription = "search")
             }
-            IconButton(onClick = {  }) {
-                Icon(imageVector = Icons.Filled.Notifications, contentDescription = "search")
+            IconButton(onClick = { onClickIcon("Notificaciones") }) {
+                Icon(imageVector = Icons.Filled.Notifications, contentDescription = "notifies")
             }
         }
     )
