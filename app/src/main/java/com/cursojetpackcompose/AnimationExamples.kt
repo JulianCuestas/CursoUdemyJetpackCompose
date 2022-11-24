8adcea2d01e28c9c8f08ef1f0563ae73ac7d8cf2
Julian Cuestas Jimenez <julian.cuestas@sii-group.co>
2022-11-24 15:11:44 -0500

    Se agrega animación de color con tiempo en la transición

--- app/src/main/java/com/cursojetpackcompose/AnimationExamples.kt
+++ app/src/main/java/com/cursojetpackcompose/AnimationExamples.kt
@@ -1,6 +1,7 @@
 package com.cursojetpackcompose
 
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -10,7 +11,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ColorSimpleAnimation() {
@@ -27,17 +30,32 @@ fun ColorSimpleAnimation() {
 
         Spacer(modifier = Modifier.height(50.dp))
 
+        var activeChangeText  by rememberSaveable { mutableStateOf(false) }
+
         var secondColor by rememberSaveable { mutableStateOf(false) }
-        val realColor2 by animateColorAsState(targetValue = if (secondColor) Color.Blue else Color.Black)
+        val realColor2 by animateColorAsState(
+            targetValue = if (secondColor) Color.Blue else Color.Red,
+            animationSpec = tween(durationMillis = 1500),// Tiempo de transición
+            finishedListener = { activeChangeText = !activeChangeText }
+        )
 
         Box(modifier = Modifier
             .size(100.dp)
             .background(realColor2)
             .clickable { secondColor = !secondColor }) {
-            Text(
-                text = "Animación",
-                modifier = Modifier.align(Alignment.Center)
-            )
+            if (activeChangeText) {
+                Text(
+                    text = "FINNN...",
+                    modifier = Modifier.align(Alignment.Center),
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            } else {
+                Text(
+                    text = "Animación",
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
         }
 
     }
